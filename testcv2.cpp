53d906af6c76550b8d91af0125b2e0b7e555f1b0
qitas <qitas@qitas.cn>
2018-09-06 15:38:07 +0800

    add start.sh

--- testcv2.cpp
+++ testcv2.cpp
@@ -1,17 +1,18 @@
 #include <iostream> 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
+using namespace std;
 using namespace cv;
 int main()
 {
 	VideoCapture cap(0);           //打开摄像头
-	// 如果要打开本地视频采用  VideoCapture cap("***.avi");
+	//VideoCapture cap("***.avi"); // 如果要打开本地视频采用  
 	if(!cap.isOpened())
 	{
-		std::cout<<"fail to open!"<<std::endl;
+		cout<<"fail to open!"<<endl;
 		return -1;     //检测一下摄像头是否打开
 	}  
-	Mat frame;  
+	Mat frame; 
 	while(1){
 		cap>>frame;        //读取当前帧
 		// 此处可添加图像处理算法，对图像进行处理，当然了，我们可以不做任何操作，只打开一下摄像头
